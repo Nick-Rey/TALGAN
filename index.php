1225cad11482265fa548bf12dc37d78f7df37691
nickrey <nickrey49@gmail.com>
2020-12-08 17:23:55 +0700

    Изменения в header добавлено изображения background.svg

--- index.php
+++ index.php
@@ -5,9 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="CSS/mainpagestyle.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
+      <nav class="header_navigation">
+           <nav class="nav_page"><a href="@">Главная</a></nav>
+           <nav class="nav_page"><a href="@">Рецепты</a></nav>
+           <nav class="nav_page"><a href="@">Заказать</a></nav>
+           <nav class="nav_page"><a href="@">О производстве</a></nav>
+           <nav class="nav_page"><a href="@">Контакты</a></nav>
+      </nav>
+      <div class="header_describe">
+         <div class="header_describe_email">paulbor@mail.ru</div>
+         <div class="header_describe_number">+7(913)449-53-31</div>
+         <div class="header_describe_name">Боргояков Павел Геннадьевич</div>
+      </div>
     </header>
+    
 </body>
 </html>

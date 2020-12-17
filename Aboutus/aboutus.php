954a6a574cd2447408b6bb63e5c5fa56e2d4718e
nickrey <nickrey49@gmail.com>
2020-12-17 13:58:42 +0700

    Закончена верстка страницы О ПРОИЗВОДСТВЕ под разрешение 1366x768

---
+++ Aboutus/aboutus.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="stylesheet" href="CSS/mainstyleaboutus.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+ </head>
+ <body>
+   <header>
+      <nav class="header_navigation">
+           <nav class="nav_page"><a href="http://talgan/">Главная</a></nav>
+           <nav class="nav_page"><a href="http://talgan/Recepts/recepts.php">Рецепты</a></nav>
+           <nav class="nav_page"><a href="@">Заказать</a></nav>
+           <nav class="nav_page"><a href="http://talgan/Aboutus/aboutus.php">О производстве</a></nav>
+           <nav class="nav_page"><a href="http://talgan/Contacts/Contacts.php">Контакты</a></nav>
+      </nav>
+      <div class="bl_for_header_describe">
+      <div class="header_describe">
+         <div class="header_describe_email">paulbor@mail.ru</div>
+         <div class="header_describe_number">+7(913)449-53-31</div>
+         <div class="header_describe_name">Боргояков Павел Геннадьевич</div>
+      </div>
+      </div>
+    </header>
+    <div class="background"></div>
+
+    <div class="production_block">
+         <div class="production_block_leftblock">
+           <div class="production_block_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat gravida sit ullamcorper nullam fringilla sagittis at aliquam. Vel massa eget quam sagittis, eu est. Sollicitudin dictumst ullamcorper diam elementum facilisi vitae quisque molestie. A, leo posuere rhoncus, eu. Sed urna, habitant arcu dolor posuere tincidunt. At amet, iaculis sit luctus eu curabitur eget. </div>
+         </div>
+         <div class="production_block_rightblock">
+             <img class="production_block_picture1" src="CSS/image/picture/millstones.jpg">
+         </div>
+    </div>
+
+    <div class="production_block">
+         <div class="production_block_leftblock">
+             <img class="production_block_picture2" src="CSS/image/picture/grind.jpg">
+          
+         </div>
+         <div class="production_block_rightblock">
+              <div class="production_block_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat gravida sit ullamcorper nullam fringilla sagittis at aliquam. Vel massa eget quam sagittis, eu est. Sollicitudin dictumst ullamcorper diam elementum facilisi vitae quisque molestie. A, leo posuere rhoncus, eu. Sed urna, habitant arcu dolor posuere tincidunt. At amet, iaculis sit luctus eu curabitur eget. </div>
+         </div>
+    </div>
+
+    <div class="production_block">
+         <div class="production_block_leftblock">
+           <div class="production_block_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat gravida sit ullamcorper nullam fringilla sagittis at aliquam. Vel massa eget quam sagittis, eu est. Sollicitudin dictumst ullamcorper diam elementum facilisi vitae quisque molestie. A, leo posuere rhoncus, eu. Sed urna, habitant arcu dolor posuere tincidunt. At amet, iaculis sit luctus eu curabitur eget. </div>
+         </div>
+         <div class="production_block_rightblock">
+             <img class="production_block_picture3" src="CSS/image/picture/product.jpg">
+         </div>
+    </div>
+
+    <footer>
+      <div class="footer_describe">Глава КФХ Боргояков Павел Геннадьевич <br> ИНН 190116626196 • ОГРНИП 315190200000650</div>
+       <div class="footer_social">
+         <div class="footer_social_block_for_icon">
+         <img src="CSS/image/social/whatsapp.png" alt="" class="footer_social_icon">
+         <img src="CSS/image/social/vk.png" alt="" class="footer_social_icon">
+         <img src="CSS/image/social/inst.png" alt="" class="footer_social_icon">
+         </div>
+         <div class="talgan19">talgan19</div>
+       </div>
+      <div class="footer_geo">655700, Республика Хакасия<br> с. Аскиз, ул. Калинина, 38</div>
+    </footer>
+ </body>
+</html>
